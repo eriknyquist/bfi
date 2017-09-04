ae9be487acc16a1e794f4ddc5b8aff83a1766774
Erik Nyquist <eknyquist@gmail.com>
2017-09-04 10:12:52 -0700

    Optimising: pre-process BF source into IR opcodes
    Overall, this massively improves performance across the board. Towers of
    Hanoi (hanoi.b) now completes in just under 3 minutes (2mins 56 secs).
    
    More specifically, compiling BF to opcodes like this allows us to:
    
    - Avoid doing the "find_dupes_ahead" processing on-the-fly
    
    - Strip out whitespace so we don't have to deal with those extra
      characters when executing BF
    
    - Identify common constructs, like clear-loops and
      copy-loops, so we can represent them with a more efficient IR opcode

--- bfi/__init__.py
+++ bfi/__init__.py
@@ -2,68 +2,41 @@ import os
 import sys
 import time
 
+OPCODE_LEFT   = 0
+OPCODE_RIGHT  = 1
+OPCODE_ADD    = 2
+OPCODE_SUB    = 3
+OPCODE_OPEN   = 4
+OPCODE_CLOSE  = 5
+OPCODE_INPUT  = 6
+OPCODE_OUTPUT = 7
+OPCODE_CLEAR  = 8
+OPCODE_COPY   = 9
+
+opcode_map = {
+    "<": OPCODE_LEFT,
+    ">": OPCODE_RIGHT,
+    "+": OPCODE_ADD,
+    "-": OPCODE_SUB,
+    "[": OPCODE_OPEN,
+    "]": OPCODE_CLOSE,
+    ",": OPCODE_INPUT,
+    ".": OPCODE_OUTPUT,
+}
+
 class BrainfuckSyntaxError(Exception):
     pass
 
 class BrainfuckMemoryError(Exception):
     pass
 
+class Opcode(object):
+    def __init__(self, code, value=None):
+        self.code = code
+        self.value = value
+
 def raise_unmatched(brace):
     raise BrainfuckSyntaxError("Error: unmatched '" + brace + "' symbol")
-
-def create_jump_table(chars):
-    table = {}
-    left_positions = []
-
-    for i in range(len(chars)):
-        if chars[i] == '[':
-            left_positions.append(i)
-
-        elif chars[i] == ']':
-            if len(left_positions) == 0:
-                raise_unmatched(']')
-
-            left = left_positions.pop()
-            table[left] = i
-            table[i] = left
-
-    if len(left_positions) != 0:
-        raise_unmatched('[')
-
-    return table
-
-class Control:
-    def __init__(self, tape_size):
-        self.tape = bytearray(tape_size)
-        self.size = tape_size
-        self.i = 0
-
-    def __checkIndex(self):
-        if self.i < 0 or self.i >= self.size:
-            raise BrainfuckMemoryError("Can't access memory at cell %d, must "
-                "be within range 0-%d" % (self.i, self.size - 1))
-
-    def incrementPointer(self, num=1):
-        self.i += num
-
-    def decrementPointer(self, num=1):
-        self.i -= num
-
-    def incrementData(self, num=1):
-        self.__checkIndex()
-        self.tape[self.i] = (self.tape[self.i] + num) % 256
-
-    def decrementData(self, num=1):
-        self.__checkIndex()
-        self.tape[self.i] = (self.tape[self.i] - num) % 256
-
-    def get(self):
-        self.__checkIndex()
-        return self.tape[self.i]
-
-    def put(self, intVal):
-        self.__checkIndex()
-        self.tape[self.i] = intVal
 
 def count_dupes_ahead(string, index):
     ret = 0
@@ -76,6 +49,160 @@ def count_dupes_ahead(string, index):
 
     return ret
 
+def is_copyloop(program, size, index):
+    if (size - index - 1) < 20:
+        MAX_COPYLOOP_LEN = size - index - 1
+    else:
+        MAX_COPYLOOP_LEN = 20
+
+    if MAX_COPYLOOP_LEN < 2 or program[index + 1] != "-":
+        return 0, 0
+
+    i = 0
+    depth = 0
+    index += 2
+
+    while (i < MAX_COPYLOOP_LEN) and (program[i:i + 2] == ">+"):
+        depth += 1
+        i += 2
+
+    if depth == 0:
+        return 0, 0
+
+    maxdepth = depth
+
+    while i < MAX_COPYLOOP_LEN:
+        if program[i] == "]" and depth == 0:
+            return maxdepth, i
+
+        elif program[i] != "<":
+            return 0, 0
+
+        depth -= 1
+        i += 1
+
+    return 0, 0
+
+def parse(program):
+    left_positions = []
+    opcodes = []
+
+    program = ''.join(program.split())
+    size = len(program)
+
+    i = 0
+    while i < size:
+        if program[i] not in opcode_map:
+            i += 1
+            continue
+
+        opcode = opcode_map[program[i]]
+
+        if opcode == OPCODE_OPEN:
+            # Optimise for 'clear cell value to 0' construct
+            if i <= (size - 2):
+                clr = program[i : i + 3]
+                if clr == "[-]" or clr == "[+]":
+                    opcodes.append(Opcode(OPCODE_CLEAR))
+                    i += 3
+                    continue
+
+            # Optimise for copy-loop construct
+            copies, chars = is_copyloop(program, size, i)
+            if copies > 0:
+                opcodes.append(Opcode(OPCODE_COPY, range(1, copies + 1)))
+                opcodes.append(Opcode(OPCODE_CLEAR))
+                i += chars + 1
+                continue
+
+            left_positions.append(len(opcodes))
+            opcodes.append(Opcode(OPCODE_OPEN))
+
+        elif opcode == OPCODE_CLOSE:
+            if len(left_positions) == 0:
+                raise_unmatched("]")
+
+            left = left_positions.pop()
+            right = len(opcodes)
+            opcodes[left].value = right
+            opcodes.append(Opcode(OPCODE_CLOSE, value=left))
+
+        elif opcode == OPCODE_INPUT or opcode == OPCODE_OUTPUT:
+            opcodes.append(Opcode(opcode_map[program[i]]))
+
+        else:
+            num = count_dupes_ahead(program, i)
+            opcodes.append(Opcode(opcode_map[program[i]], value=num + 1))
+            i += num
+
+        i += 1
+
+    if len(left_positions) != 0:
+        raise_unmatched('[')
+
+    return opcodes
+
+class Control:
+    def __init__(self, tape_size):
+        self.tape = bytearray(tape_size)
+        self.size = tape_size
+        self.i = 0
+
+    def __checkIndex(self, i):
+        if i < 0 or i >= self.size:
+            raise BrainfuckMemoryError("Can't access memory at cell %d, must "
+                "be within range 0-%d" % (i, self.size - 1))
+
+    def incrementPointer(self, num=1):
+        self.i += num
+
+    def decrementPointer(self, num=1):
+        self.i -= num
+
+    def incrementData(self, num=1):
+        self.__checkIndex(self.i)
+        self.tape[self.i] = (self.tape[self.i] + num) % 256
+
+    def decrementData(self, num=1):
+        self.__checkIndex(self.i)
+        self.tape[self.i] = (self.tape[self.i] - num) % 256
+
+    def clearData(self):
+        self.__checkIndex(self.i)
+        self.tape[self.i] = 0
+
+    def copyData(self, *offs):
+        self.__checkIndex(self.i)
+        for off in offs:
+            index = self.i + off
+            self.__checkIndex(index)
+            self.tape[index] = self.tape[self.i]
+
+    def get(self):
+        self.__checkIndex(self.i)
+        return self.tape[self.i]
+
+    def put(self, intVal):
+        self.__checkIndex(self.i)
+        self.tape[self.i] = intVal
+
+def print_opcodes(opcodes):
+    name_map = {
+        OPCODE_LEFT: "left",
+        OPCODE_RIGHT: "right",
+        OPCODE_ADD: "add",
+        OPCODE_SUB: "sub",
+        OPCODE_OPEN: "open",
+        OPCODE_CLOSE: "close",
+        OPCODE_INPUT: "input",
+        OPCODE_OUTPUT: "output",
+        OPCODE_CLEAR: "clear",
+        OPCODE_COPY: "copy"
+    }
+
+    for op in opcodes:
+        print "%s %s" % (name_map[op.code], op.value)
+
 def interpret(program, stdin=None, time_limit=None, tape_size=30000,
               buffer_stdout=False):
     if not isinstance(program, basestring):
@@ -86,9 +213,12 @@ def interpret(program, stdin=None, time_limit=None, tape_size=30000,
     if stdin != None:
         stdin = list(reversed(stdin))
 
-    size = len(program) # number of characters in the program
-    ret = []            # program output
-    i = 0               # index of current character in program
+    opcodes = parse(program)   # IR opcodes
+    size = len(opcodes)        # number of opcodes in IR
+    ret = []                   # program output
+    i = 0                      # index of current character in program
+
+    #print_opcodes(opcodes)
 
     def write_stdout(c):
         os.write(1, c)
@@ -110,44 +240,39 @@ def interpret(program, stdin=None, time_limit=None, tape_size=30000,
     do_write = write_buf if buffer_stdout else write_stdout
     do_read = read_stdin if stdin == None else read_buf
 
-    jump_table = create_jump_table(program)
-
     if time_limit != None:
         start = time.time()
 
     while i < size:
-        c = program[i]
+        op = opcodes[i]
 
-        if c == '>':
-            num = count_dupes_ahead(program, i)
-            ctrl.incrementPointer(num + 1)
-            i += num
+        if op.code == OPCODE_RIGHT:
+            ctrl.incrementPointer(op.value)
 
-        elif c == '<':
-            num = count_dupes_ahead(program, i)
-            ctrl.decrementPointer(num + 1)
-            i += num
+        elif op.code == OPCODE_LEFT:
+            ctrl.decrementPointer(op.value)
 
-        elif c == '+':
-            num = count_dupes_ahead(program, i)
-            ctrl.incrementData(num + 1)
-            i += num
+        elif op.code == OPCODE_ADD:
+            ctrl.incrementData(op.value)
 
-        elif c == '-':
-            num = count_dupes_ahead(program, i)
-            ctrl.decrementData(num + 1)
-            i += num
+        elif op.code == OPCODE_SUB:
+            ctrl.decrementData(op.value)
 
-        elif c == '[' and ctrl.get() == 0:
-            i = jump_table[i]
+        elif op.code == OPCODE_CLEAR:
+            ctrl.clearData()
 
-        elif c == ']' and ctrl.get() != 0:
-            i = jump_table[i]
+        elif op.code == OPCODE_COPY:
+            ctrl.copyData(*op.value)
+        elif op.code == OPCODE_OPEN and ctrl.get() == 0:
+            i = op.value - 1
 
-        elif c == '.':
+        elif op.code == OPCODE_CLOSE and ctrl.get() != 0:
+            i = op.value - 1
+
+        elif op.code == OPCODE_OUTPUT:
             do_write(chr(ctrl.get()))
 
-        elif c == ',':
+        elif op.code == OPCODE_INPUT:
             ch = do_read()
             if len(ch) > 0 and ord(ch) > 0:
                 ctrl.put(ord(ch))
